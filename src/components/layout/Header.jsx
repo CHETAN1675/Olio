1c6a2effa974810b4b19770cd7123a77b3674073
CHETAN1675 <mahajanchetan001@gmail.com>
2026-04-30 12:34:52 +0530

    Logo path Change

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,11 @@
+import Vector from "../../assets/Vector.png"
 export default function Header() {
   return (
     <header className="w-full flex items-center justify-between h-[73px] bg-white border-b border-gray-200">
       
       {/* Logo */}
       <div className="flex items-center">
-        <img src="/src/assets/Vector.png" alt="Olio Maximus" className="h-6 w-auto" />
+        <img src={Vector} alt="Olio Maximus" className="h-6 w-auto" />
       </div>
 
       {/* Right Section */}
